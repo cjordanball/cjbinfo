f81ad4af49d54faf7c1bbbc608fb3df5d5491cb8
C. Jordan Ball III <cjordanball@gmail.com>
2017-06-07 08:52:33 -0400

    add redux cycle and test AJAX call

---
+++ src/actions/types.js
@@ -0,0 +1,6 @@
+const ActionTypes = {
+	TEST_FUNCTION: 'TEST_FUNCTION',
+	AUTH_ERROR: 'AUTH_ERROR'
+};
+
+export default ActionTypes;
--- src/components/app.js
+++ src/components/app.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import construct from '../../assets/images/underConstruction.jpg';
+import * as actions from '../actions';
 
-const App = () => (
-	<div>
-		<div className="heading">
-			<h2>You Have Arrived at the Website of</h2>
-			<h1>C. Jordan Ball III</h1>
-			<h2>Please Forgive My Disarray!</h2>
-		</div>
-		<img alt="construction site" className="placeholdImage" src={construct} />
-		<a href="http://www.freepik.com/free-photo/warning-zone_953380.htm">Designed by Freepik</a>
-		<div>
-			<p>
-				I recently decided to update this website from front to back, to clear out
-				accumulated clutter and to make it more enjoyable and helpful for the user.
-				Currently, I expect to begin placing materials onto the page in the first
-				week of June.  Thank you for your patience.
-			</p>
-		</div>
-	</div>
-);
+class App extends Component {
+	render() {
+		console.log('props', this.props);
+		return (
+			<div>
+				<div className="heading">
+					<h2>You Have Arrived at the Website of</h2>
+					<h1>C. Jordan Ball III</h1>
+					<h2>Please Forgive My Disarray!</h2>
+					<button onClick={() => this.props.testFunc()}>Press Me</button>
+					<div>
+						<h3>
+							{this.props.message ? this.props.message : ''}
+						</h3>
+					</div>
+				</div>
+				<img alt="construction site" className="placeholdImage" src={construct} />
+				<a href="http://www.freepik.com/free-photo/warning-zone_953380.htm">Designed by Freepik</a>
+				<div>
+					<p>
+						I recently decided to update this website from front to back, to clear out
+						accumulated clutter and to make it more enjoyable and helpful for the user.
+						Currently, I expect to begin placing materials onto the page in the first
+						week of June.  Thank you for your patience.
+					</p>
+				</div>
+			</div>
+		);
+	}
+}
 
-export default App;
+const mapStateToProps = (state) => {
+	console.log('in MSTP', state);
+	return {
+		message: state.test.message
+	};
+};
+
+export default connect(mapStateToProps, actions)(App);
