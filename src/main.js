6dc1d8d0c851709b486d3020a962978d5c4ee2e6
C. Jordan Ball III <cjordanball@gmail.com>
2017-06-13 12:17:52 -0400

    Check for token on hitting init page

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import reducers from './reducers';
 import '../styles/styles.less';
+import ActionTypes from './actions/Types';
 import Header from './components/common/header';
 import FrontPage from './components/frontPage/frontPage';
 import SignInPage from './components/authComponents/signin';
@@ -15,9 +16,14 @@ import HomePage from './components/home';
 import LogOutPage from './components/authComponents/signout';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+const token = localStorage.getItem('token');
+if (token) {
+	store.dispatch({ type: ActionTypes.AUTH_USER });
+}
 
 ReactDOM.render(
-	<Provider store={createStoreWithMiddleware(reducers)}>
+	<Provider store={store}>
 		<BrowserRouter>
 			<div className="container">
 				<Header />
--- src/reducers/auth_reducer.js
+++ src/reducers/auth_reducer.js
@@ -8,6 +8,8 @@ export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
 	case ActionTypes.SIGN_IN_USER:
 		return { ...state, logStatus: true };
+	case ActionTypes.AUTH_USER:
+		return { ...state, logStatus: true };
 	case ActionTypes.SIGN_UP_USER:
 		return { ...state, logStatus: true };
 	case ActionTypes.LOG_OUT_USER:
