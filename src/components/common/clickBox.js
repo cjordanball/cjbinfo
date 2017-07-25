d6418dccfe4b6bde07f0b6dcf5e0aef6e335f892
C. Jordan Ball III <cjordanball@gmail.com>
2017-07-24 22:16:13 -0400

    add outlines to website

--- src/components/common/clickBox.js
+++ src/components/common/clickBox.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import sudokuImage from '../../../assets/images/su_doku.jpg';
+import outlineImage from '../../../assets/images/ng4Outline.png';
 
 const ClickBoxComponent = props => (
 	<Link to={props.target}>
 		<div className="clickBox">
-			<div className="pictureBox" />
+			{props.imageName === "sudoku" ?
+				<div style={{backgroundImage: `url(${sudokuImage})`}} className="pictureBox" /> : null}
+			{props.imageName === "outlines" ?
+				<div style={{backgroundImage: `url(${outlineImage})`}} className="pictureBox" /> : null}
 			<div className="text">
 				{props.title}
 			</div>
---
+++ src/components/outlines/outlines.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const OutlinesPage = () => (
+	<div>
+		<ul>
+			<h3>
+				<li>
+					<a target="blank" href="https://s3.amazonaws.com/cjbinfo-docs/React+Summary.html">React</a>
+				</li>
+				<li>
+					<a target="blank" href="https://s3.amazonaws.com/cjbinfo-docs/Angular4+Notes.html">Angluar 4</a>
+				</li>
+				<li>
+					<a target="blank" href="https://s3.amazonaws.com/cjbinfo-docs/MongoOutline.html">Mongo/Mongoose</a>
+				</li>
+				<li>
+					<a target="blank" href="https://s3.amazonaws.com/cjbinfo-docs/Docker+and+CI.html">Docker</a>
+				</li>
+				<li>
+					<a target="blank" href="https://s3.amazonaws.com/cjbinfo-docs/Introduction+to+Webpack.html">Webpack</a>
+				</li>
+				<li>
+					<a target="blank" href="https://s3.amazonaws.com/cjbinfo-docs/Regular+Expressions+in+Javascript.html">Regular Expressions</a>
+				</li>
+				<li>
+					<a target="blank" href="https://s3.amazonaws.com/cjbinfo-docs/Generators.html">Asynchronous Code and Generators</a>
+				</li>
+			</h3>
+		</ul>
+	</div>
+
+);
+
+export default OutlinesPage;
